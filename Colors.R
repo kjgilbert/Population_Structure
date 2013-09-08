83ac8bed3604f1398d1b5a880942aad3740bc680
kjgilbert <kgilbert@zoology.ubc.ca>
2013-09-07 22:13:17 -0700

    add pertinent files

---
+++ Colors.R
@@ -0,0 +1,17 @@
+#make default color palettes
+
+
+
+
+
+#these currently set up to work with bar plots
+colors <- c("khaki", "darkblue", "steelblue2", "darkorange", "green4", "red2", "purple", "yellow")
+	#can add more colors if k>8, or can alter defaults as desired
+bw <- c("black", "gray95", "gray50", "gray25", "gray75", "gray40", "gray85")
+	#black and white color palette option
+
+
+#make a set of transparent colors as well:
+library(scales)
+tcolors <- alpha(colors, alpha=0.5)
+tbw <- alpha(bw, alpha=0.5)
---
+++ MakeBarPlots.R
@@ -0,0 +1,21 @@
+## make a barplot from population structure data
+
+source('ReadQ.R')
+source('PlotQ.R')
+
+
+# read in the "indfile" that is output from clumpp here, this is for the example file provided
+clumppfile <- "K3_indfile"
+
+#function to format and read the data, uses ReadQ.R 
+read.q(clumppfile, clumpp=TRUE)
+
+#function to create the bar plot from the formatted data, uses PlotQ.R 
+plot.bars(bars, type="quartz")#, ax.lab=FALSE)
+
+
+
+
+
+
+## code for sizing window and other labelling details etc in progress,  7 Sep 2013
